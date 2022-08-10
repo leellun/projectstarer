a6b689c6465b061ded5115f87e67704094116299
leellun <leellun@sina.cn>
2022-08-11 01:09:40 +0800

    cors

---
+++ security/src/main/java/com/newland/security/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.newland.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class WebMvcConfig {
+    /**
+     * 跨域访问 不用每一个controller 都去注释 @CrossOrigin
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        CorsFilter filter = new CorsFilter(source);
+        return filter;
+    }
+}
